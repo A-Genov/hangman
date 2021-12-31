35429341f163181d31ec32ed90854c635b5afab7
A-Genov <80156452+A-Genov@users.noreply.github.com>
2021-12-31 20:23:58 +0200

    show wrong letters, show parts & restart game functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
@@ -11,7 +11,7 @@ const words = ['application', 'programming', 'interface', 'algorithm', 'conditio
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['a', 'l', 'g', 'o', 'r', 'i', 't', 'h', 'm'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
@@ -37,7 +37,28 @@ function displayWord() {
 
 // Update wrong letters
 function updateWrongLettersEl() {
-    console.log('Update wrong');
+    // display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong Letters</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    // display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    })
+
+    // check if lost
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunatelly you lost :(';
+        popup.style.display = 'flex';
+    }
 }
 
 // Show notification
@@ -45,7 +66,7 @@ function showNotification() {
     notification.classList.add('show');
 
     setTimeout(() => {
-        notification.classList.add('remove');
+        notification.classList.remove('show');
     }, 2000)
 }
 
@@ -70,6 +91,20 @@ window.addEventListener('keydown', e => {
             }
         }
     }
+});
+
+// Restart game
+playAgainBtn.addEventListener('click', () => {
+    // empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = 'none';
 })
 
 displayWord();
