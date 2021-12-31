0caa69240ec16ddc41076b14be51eac75a94e506
A-Genov <80156452+A-Genov@users.noreply.github.com>
2021-12-31 19:45:51 +0200

    display word functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'algorithm', 'condition', 'abstraction', 'automation', 'architecture', 'concatenation', 'cryptography', 'debugging', 'gigabyte', 'inheritance', 'environment', 'prototyping', 'variable'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ['a', 'l', 'g', 'o', 'r', 'i', 't', 'h', 'm'];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+        ${selectedWord
+            .split('')
+            .map(letter => `
+                <span class="letter">
+                    ${correctLetters.includes(letter) ? letter : ''}
+                </span>
+            `)
+            .join('')}
+    `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+    if(innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won! :)';
+        popup.style.display = 'flex';
+    }
+}
+
+displayWord();
